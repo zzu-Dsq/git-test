7967ca19434b21b4ec656cb8d3cf782e90a9503e
dsq <2911993665@qq.com>
2021-09-27 10:23:46 +0800

    first commit

---
+++ src/main/java/com/dsq/Hello.java
@@ -0,0 +1,15 @@
+package com.dsq;
+
+/**
+ * ClassName: Hello
+ * Package: com.dsq
+ * Description:
+ *
+ * @Data:2021/9/27 10:20
+ * @Author: dsq
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
